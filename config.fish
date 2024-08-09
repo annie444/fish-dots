5badb2c59e3c3cae9e12d8cede3f7017ab710a79
Annie Ehler <annie.ehler.4@gmail.com>
2024-08-09 09:15:44 -0700

    Update the FZF and BAT configs

--- config.fish
+++ config.fish
@@ -12,85 +12,52 @@ status --is-interactive; and begin
 
     set -gx GPG_TTY (tty)
     thefuck --alias | source
-    set -g fish_greeting ""
+    batman --export-env | source
+    eval (batpipe)
 
-    function cd -w z -d "alias cd z"
-      z $argv
-    end
-    function .. -d "alias .. 'z ..'"
-      z ..
-    end
-    function :q -w exit -d "alias :q exit"
-      exit
-    end
-    function cat -w bat -d "alias cat bat"
-      bat $argv
-    end
-    function du -w dust -d "alias du dust"
-      dust $argv
-    end
-    function g -w git -d 'alias g git'
-      git $argv
-    end
-    function ls -w eza -d "alias ls 'eza -1GghmMoXr --color=always --icons=always -s created --group-directories-first --time-style long-iso --git --git-repos --hyperlink --show-symlinks -w 10'"
-      eza -1GghmMoXr --color=always --icons=always -s created --group-directories-first --time-style long-iso --git --git-repos --hyperlink --show-symlinks -w 10 $argv
-    end
-    function la -w eza -d "alias la 'eza -1GghmMoXrla --color=always --icons=always -s created --group-directories-first --time-style long-iso --git --git-repos --hyperlink --show-symlinks -w 100'"
-      eza -1GghmMoXrla --color=always --icons=always -s created --group-directories-first --time-style long-iso --git --git-repos --hyperlink --show-symlinks -w 100 $argv
-    end
-    function vim -w nvim -d "alias vim nvim"
-      nvim $argv
-    end
-    function vi -w nvim -d "alias vi nvim"
-      nvim $argv
-    end
-    function tp -w trash-put -d "alias tp trash-put"
-      trash-put $argv
-    end
-    function te -w trash-empty -d "alias te trash-empty"
-      trash-empty $argv
-    end
-    function tl -w trash-list -d "alias tl trash-list"
-      trash-list $argv
-    end
-    function tre -w trash-restore -d "alias tre trash-restore"
-      trash-restore $argv
-    end
-    function trm -w trash-rm -d "alias trm trash-rm"
-      trash-rm $argv
-    end
-    function vimdiff -w nvim -d "alias vimdiff nvim -d"
-      nvim -d $argv
-    end
-    function diff -w batdiff -d "alias diff batdiff"
-      nvim -d $argv
-    end
-    function find -w bfs -d "alias find bfs"
-      bfs $argv
-    end
-    function gd -w batdiff -d "alias gd batdiff"
-      batdiff $argv
-    end
-    function bathelp -w bat -d "alias bathelp 'bat --plain --language=help'"
-      bat --plain --language=help $argv
-    end
-    function ssh -d "alias ssh kitten ssh"
-      kitten ssh $argv
-    end
-    function note -w nvim -d "alias note 'nvim -c ':ObsidianToday<CR>'"
-      nvim -c ':ObsidianToday<CR>' $argv
-    end
+    abbr -a du dust
+    abbr -a g git
+    abbr -a vim nvim
+    abbr -a vi nvim
+    abbr -a diff batdiff
+    abbr -a vimdiff "nvim -d"
+    abbr -a gd batdiff
+    abbr -a bathelp "bat --plain --language=help"
+    abbr -a note "nvim -c ':ObsidianToday<CR>'"
+    abbr -a ":q" exit
+    abbr -a k kubectl
+    abbr -a cat bat
+    abbr -a tp trash-put
+    abbr -a te trash-empty
+    abbr -a tl trash-list
+    abbr -a tre trash-restore
+    abbr -a trm trash-rm
+    abbr -a grep batgrep
+    abbr -a watch batwatch
+
+    alias cd z
+    alias .. 'z ..'
+    alias ls 'eza -1GghmMoXr --color=always --icons=always -s created --group-directories-first --time-style long-iso --git --git-repos --hyperlink --show-symlinks -w 10'
+    alias la 'eza -1GghmMoXrla --color=always --icons=always -s created --group-directories-first --time-style long-iso --git --git-repos --hyperlink --show-symlinks -w 100'
+    alias ssh kitten ssh
     alias esp ". $HOME/esp/esp-idf/export.fish"
-    
 
     if test "$(uname)" = "Darwin"
       alias apptainer "limactl shell apptainer"
     end
     set -gx fish_greeting ""
-    set -gx fzf_preview_dir eza --all --color=always
-    set -gx fzf_preview_file bat
+
+    # FZF Config
+    # from https://github.com/PatrickF1/fzf.fish#change-fzf-options-for-a-specific-command
+    set -gx fzf_preview_dir_cmd 'tre -C {} | head -n 200'
+    set -gx fzf_preview_file_cmd 'bat --color=always --style=numbers --line-range=:500s {}'
+    set -gx fzf_diff_highlighter 'batdiff --paging=never --width=20'
+    set -gx fzf_history_time_format "%Y-%m-%d %H:%M:%S"
     set -gx fzf_fd_opts --hidden
-    set -gx fzf_diff_highlighter delta --paging=never --width=20
+
+    # from https://github.com/junegunn/fzf?tab=readme-ov-file#environment-variables
+    set -gx FZF_DEFAULT_COMMAND 'bfs . -type f -name'
+    
 
     if set -q KITTY_INSTALLATION_DIR
       source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
@@ -130,26 +97,15 @@ status --is-interactive; and begin
       set -gx PATH "/usr/local/texlive/2024/bin/x86_64-linux" $PATH
     end
 
-    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
-    set -gx MANROFFOPT "-c"
     set -gx BAT_THEME "Dracula"
     set -gx XDG_CONFIG_HOME "$HOME/.config"
     set -gx XDG_CACHE_HOME "$HOME/.cache"
     set -gx COLORTERM "truecolor"
     set -gx TERM "xterm-256color"
     set -gx EDITOR "nvim"
-    
-    if test -d "$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t"
-      set -gx SSH_AUTH_SOCK "~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-    else if test -d "$HOME/.1password"
-      set -gx SSH_AUTH_SOCK "~/.1password/agent.sock"
-    end
-
-    if test -d /home/linuxbrew
-      /home/linuxbrew/.linuxbrew/bin/brew shellenv fish | source
-    else if test -d /opt/homebrew
-      /opt/homebrew/bin/brew shellenv | source
-    end
+    set -gx PAGER "less"
+    set -gx BATPIPE "color"
+    set -gx BATDIFF_USE_DELTA true
 
     if test -d "$HOME/.asdf"
       source ~/.asdf/asdf.fish
