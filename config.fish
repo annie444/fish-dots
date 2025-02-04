9d1f0ab3e803aabcdaa7bea9821465716213ae62
Annie Ehler <annie.ehler.4@gmail.com>
2025-02-04 12:09:37 -0800

    Update the asdf config

--- config.fish
+++ config.fish
@@ -160,11 +160,7 @@ if test "$(uname)" = "Darwin"
 end
 
 # ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
+set _asdf_shims "$ASDF_DATA_DIR/shims"
 
 # Do not use fish_add_path (added in Fish 3.2) because it
 # potentially changes the order of items in PATH
