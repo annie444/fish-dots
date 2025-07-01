87bc57f02b56b2788847e4be6f92d9e1951f454c
Annie Ehler <annie.ehler.4@gmail.com>
2025-07-01 10:49:17 -0700

    [2025-07-01 10:49:17] (update fish) Committing all changes to fish-dots

--- functions/update.fish
+++ functions/update.fish
@@ -9,7 +9,11 @@ function update -a cmd -d "Update various tools"
             _update_neovim $flag_h $flag_help $argv
         case fish
             _update_fish $flag_h $flag_help $argv
+        case brew
+            _update_brew $flag_h $flag_help $argv
         case all
+            _partial_fish $flag_h $flag_help $argv
+            _partial_neovim $flag_h $flag_help $argv
             _update_chezmoi $flag_h $flag_help $argv
             _update_asdf $flag_h $flag_help $argv
         case *
@@ -25,8 +29,20 @@ function update -a cmd -d "Update various tools"
             echo "dotfiles in the `~/.dotfiles' directory then update the chezmoi repo." >&2
             return 1
     end
-    cd $_starting_dir
-    set --erase _starting_dir
+    return $status
+end
+
+function _update_brew -d "Update Homebrew"
+    argparse --name "update brew" h/help -- $argv
+    if test -n "$_flag_h" -o -n "$_flag_help"
+        echo "Updates Homebrew and all installed packages." >&2
+        return 1
+    end
+    brew cleanup
+    brew update
+    brew upgrade
+    brew autoremove
+    brew cleanup
     return $status
 end
 
@@ -37,8 +53,6 @@ function _update_chezmoi -d "Update chezmoi"
         return 1
     end
     chezmoi update --recursive --recurse-submodules
-    cd $_starting_dir
-    set --erase _starting_dir
     return $status
 end
 
@@ -51,12 +65,37 @@ function _update_asdf -d "Update asdf"
     end
     asdf plugin update --all
     set -f _update_plugins (asdf latest --all | grep missing | awk '{print $1}')
-    for plugin in $_update_plugins
-        asdf install $plugin latest
-        asdf set $plugin latest
+    set -f _update_versions (asdf latest --all | grep missing | awk '{print $2}')
+    for plugin in (seq (count $_update_plugins))
+        asdf install $_update_plugins[$plugin] $_update_versions[$plugin]
+        asdf set --home $_update_plugins[$plugin] $_update_versions[$plugin]
     end
-    cd $_starting_dir
-    set --erase _starting_dir
+    return $status
+end
+
+function _update_neovim -d "Pushes changes to nvim-dots and updates chezmoi's neovim configs"
+    argparse --name "partially update neovim" h/help -- $argv
+    if test -n "$_flag_h" -o -n "$_flag_help"
+        echo "Updates the neovim configuration by syncing" >&2
+        echo "`~/.dotfiles/nvim-dots'" >&2
+        echo "with the chezmoi nvim configs" >&2
+        return 1
+    end
+    begin
+        set -f _local_starting_dir (pwd)
+        set -f NOW (date "+%Y-%m-%d %H:%M:%S")
+        cd ~/.dotfiles/nvim-dots
+        git add -A
+        git commit -m "[$NOW] (update neovim) Committing all changes to nvim-dots"
+        git push
+        cd ~/.local/share/chezmoi/dot_config/nvim
+        git pull
+        cd ~/.local/share/chezmoi
+        git add -A
+        git commit -m "[$NOW] (update neovim) Pulling changes from nvim-dots"
+        git push
+        cd $_local_starting_dir
+    end
     return $status
 end
 
@@ -84,9 +123,32 @@ function _update_neovim -d "Update neovim"
         cd $_local_starting_dir
     end
     _update_chezmoi
-    cd $_starting_dir
-    set --erase _starting_dir
-    set --erase NOW
+    return $status
+end
+
+function _partial_fish -d "Pushes changes to fish-dots and updates chezmoi's fish configs"
+    argparse --name "partially update fish" h/help -- $argv
+    if test -n "$_flag_h" -o -n "$_flag_help"
+        echo "Updates the fish configuration by syncing" >&2
+        echo "`~/.dotfiles/fish-dots'" >&2
+        echo "with the chezmoi fish configs" >&2
+        return 1
+    end
+    begin
+        set -f _local_starting_dir (pwd)
+        set -f NOW (date "+%Y-%m-%d %H:%M:%S")
+        cd ~/.dotfiles/fish-dots
+        git add -A
+        git commit -m "[$NOW] (update fish) Committing all changes to fish-dots"
+        git push
+        cd ~/.local/share/chezmoi/dot_config/fish
+        git pull
+        cd ~/.local/share/chezmoi
+        git add -A
+        git commit -m "[$NOW] (update fish) Pulling changes from fish-dots"
+        git push
+        cd $_local_starting_dir
+    end
     return $status
 end
 
@@ -114,8 +176,5 @@ function _update_fish -d "Update fish"
         cd $_local_starting_dir
     end
     _update_chezmoi
-    cd $_starting_dir
-    set --erase _starting_dir
-    set --erase NOW
     return $status
 end
